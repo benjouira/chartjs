f2b09ed02c244702b49e742a93a329616f8c2cec
Rabii <rabi3jouira@gmail.com>
2022-03-04 23:44:16 +0100

    Update BarChart.js

--- BarChart.js
+++ BarChart.js
@@ -19,3 +19,14 @@ data: {
         data: [10, 20, 30, 40, 50, 60, 70]
     }]
 };
+
+options = {
+    scales: {
+        x: {
+            grid: {
+              offset: true
+            }
+        }
+    }
+};
+ 
