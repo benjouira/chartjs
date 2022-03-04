d257d6a5a01c44f27f89e0166ded200246ef7df6
Rabii <rabi3jouira@gmail.com>
2022-03-04 23:44:34 +0100

    Update BarChart.js

--- BarChart.js
+++ BarChart.js
@@ -30,3 +30,17 @@ options = {
     }
 };
  
+const stackedBar = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            x: {
+                stacked: true
+            },
+            y: {
+                stacked: true
+            }
+        }
+    }
+});
